b9b1a69210b691de063b1cd3f980383bc4bf9108
vaishu8747 <64781305+vaishu8747@users.noreply.github.com>
2020-07-17 22:04:58 +0530

    Update Assignment1_C.py

--- Assignment1_C.py
+++ Assignment1_C.py
@@ -1,9 +1,15 @@
 a=input("enter the first no.")
 b=input("enter the second no.")
+
+#Sum of two number
 sum=int(a)+int(b)
 print('sum of {0} and {1} is {2}'.format(a,b,sum))
+
+#Difference of two number
 diff=int(a)-int(b)
 print('difference of {0} and {1} is {2}'.format(a,b,diff))
+
+#Product of two number
 product=int(a)*int(b)
 print('Product of {0} and {1} is {2}'.format(a,b,product))
 
