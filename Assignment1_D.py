fee286f33552c7ce266c3c6d12048b6b51ef841e
vaishu8747 <64781305+vaishu8747@users.noreply.github.com>
2020-07-17 22:02:52 +0530

    Update Assignment1_D.py

--- Assignment1_D.py
+++ Assignment1_D.py
@@ -1,6 +1,10 @@
 num1=input("enter the first no.")
 num2=input("enter the second no.")
+
+#integer division
 result=int(num1)//int(num2)
 print('Division of integer no {0} and {1} is {2}'.format(num1,num2,result))
+
+#Float division
 result=int(num1)/int(num2)
 print('Division of integer no {0} and {1} is {2}'.format(num1,num2,result))
